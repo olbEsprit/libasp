a9ad75242ceef23b08754f51962ff9b08c7cad66
kn1m <peteknysh@gmail.com>
2015-07-06 13:05:29 +0300

    properly implemented tree

--- lib_asp/WebForm1.aspx
+++ lib_asp/WebForm1.aspx
@@ -7,8 +7,21 @@
     <title></title>
 </head>
 <body>
+
+
+
+
+
     <form id="form1" runat="server">
         <div>
+            <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+            <link href="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.3/css/bootstrap.min.css"
+                rel="stylesheet" type="text/css" />
+            <script type="text/javascript" src="http://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.0.3/js/bootstrap.min.js"></script>
+            <link href="http://cdn.rawgit.com/davidstutz/bootstrap-multiselect/master/dist/css/bootstrap-multiselect.css"
+                rel="stylesheet" type="text/css" />
+            <script src="http://cdn.rawgit.com/davidstutz/bootstrap-multiselect/master/dist/js/bootstrap-multiselect.js"
+                type="text/javascript"></script>
 
             <asp:TreeView ID="nodenod" runat="server" ShowLines="true" ShowCheckBoxes="All" OnTreeNodeCheckChanged="nodenod_TreeNodeCheckChanged">
                 <Nodes>
@@ -19,9 +32,7 @@
 
                         <asp:TreeNode Value="Page 1"
                             Text="Page1"
-                            Target="Content">
-
-                        </asp:TreeNode>
+                            Target="Content"></asp:TreeNode>
 
                         <asp:TreeNode Value="Page 2"
                             Text="Page 2"
@@ -39,8 +50,35 @@
                         Target="Content"></asp:TreeNode>
                 </Nodes>
             </asp:TreeView>
-            <asp:Button ID="Button1" Text="Submit" runat="server" OnClick="Submit" />
+            <br />
+
         </div>
+
+        <asp:Button ID="Button1" Text="Submit" runat="server" OnClick="Submit" />
     </form>
+
+    <script type="text/javascript">
+        $.ajax({
+            url: 'WebForm1.aspx/GetServerData',
+            type: 'POST',
+            contentType: "application/json; charset=utf-8",
+            success: function (response) {
+                $('#result').text(response.d);
+                console.log(response);
+            },
+            error: function (error) {
+                console.log(error);
+            }
+        });
+
+    </script>
+
+
+
+    <br />
+    <p>
+        Result:
+        <output id="result"></output>
+    </p>
 </body>
 </html>
--- lib_asp/WebForm1.aspx.cs
+++ lib_asp/WebForm1.aspx.cs
@@ -4,61 +4,59 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Services;
 using OriginalUserControls;
 using System.Text;
 
-
-
 namespace lib_asp
 {
-        public partial class WebForm1 : System.Web.UI.Page
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+
+        public static string message = "";
+
+        protected void Page_Load(object sender, EventArgs e)
         {
-            public string message = "";
-            protected void Page_Load(object sender, EventArgs e)
-            {
-                
-            }
 
-            protected void Submit(object sender, EventArgs e)
-            {/*
-                string message = "";
-                foreach (TreeNode t in nodenod.Nodes)
-                {
-                    if (t.Checked == true)
-                    {
-                        message += t.Text.ToString() + "; ";
-                    }
-                    if (t.ChildNodes.Count > 0)
-                    {
-                        foreach (TreeNode tt in t.ChildNodes)
-                            if (tt.Checked == true)
-                            {
-                                message += tt.Text.ToString() + "; ";
-                            }
-                    }
-                }*/
-                ClientScript.RegisterClientScriptBlock(this.GetType(), "alert", "alert('" + message + "');", true);
-            }
-
-            public void nodenod_TreeNodeCheckChanged(object sender, TreeNodeEventArgs e)
-            {
-                
-                foreach (TreeNode t in nodenod.Nodes)
-                {
-                    if (t.Checked == true)
-                    {
-                        message += t.Text.ToString() + "; ";
-                    }
-                    if (t.ChildNodes.Count > 0)
-                    {
-                        foreach (TreeNode tt in t.ChildNodes)
-                            if (tt.Checked == true)
-                            {
-                                message += tt.Text.ToString() + "; ";
-                            }
-                    }
-                }
-            }
 
         }
+            
+        
+        public void nodenod_TreeNodeCheckChanged(object sender, TreeNodeEventArgs e)
+        {
+            message = "";
+            foreach (TreeNode t in nodenod.Nodes)
+            {
+                if (t.Checked == true)
+                {
+                    message += t.Text.ToString() + "; ";
+                }
+                if (t.ChildNodes.Count > 0)
+                {
+                    foreach (TreeNode tt in t.ChildNodes)
+                        if (tt.Checked == true)
+                        {
+                            message += tt.Text.ToString() + "; ";
+
+                        }     
+                }
+            }
+           
+
+        }
+
+        public void Submit(object sender, EventArgs e)
+        {
+            // Updates checked value
+        }
+        
+
+        [WebMethod]
+        public static string GetServerData()
+        {
+            return message;
+        }
     }
+}
+    
